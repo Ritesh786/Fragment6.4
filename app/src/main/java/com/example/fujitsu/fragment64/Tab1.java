9c83b6473748ad5b1067c9200e776f0c1b832c4c
Ritesh Jha <jharitesh6@gmail.com>
2017-04-11 09:23:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fujitsu/fragment64/Tab1.java
@@ -0,0 +1,21 @@
+package com.example.fujitsu.fragment64;
+
+/**
+ * Created by Fujitsu on 26/02/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1  extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+        return rootView;
+    }
+}
